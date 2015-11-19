1edb4eff246b543ff7e6b75c2c52b4ce324c8933
Abdullah Al Masum <Abdullah Al Masum>
2015-11-19 09:52:52 +0600

    first commit

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-11-18 18:10:04 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-11-18T18:10:04+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
